275c48c014fd34f7e2a5d3fb22355854ff678fb2
Daniel Núñez Cano <danielnunezcano@gmail.com>
2024-12-30 12:38:24 +0100

    update

--- command/generate.py
+++ command/generate.py
@@ -2,9 +2,9 @@ import custom_gemini
 
 def generate_command(request,so):
     question_to_gemini = f"Dame la instrucción para terminal de {so} de lo siguiente, no me des explicaciones, solo quiero el comando y que sea en en texto plano: {request}"
-    response =  custom_gemini.generate_clean_response(question_to_gemini)
+    response =  custom_gemini.generate_response(question_to_gemini)
     return response.rstrip().replace("```", "").replace("\n", "")
 
 def generate_command_description(request,so):
     question_to_gemini = f"Dime la descripción del siguente comando de {so}: {request}"
-    return custom_gemini.generate_clean_response(question_to_gemini)
+    return custom_gemini.generate_response(question_to_gemini)
